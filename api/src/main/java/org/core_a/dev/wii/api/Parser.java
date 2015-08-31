2640a29913ef9d2c9631a2d2b3682fff3b152619
super-fish <super_fishz@core-a.org>
2015-09-01 04:28:26 +0900

    parse docuemnt fixed

--- api/src/main/java/org/core_a/dev/wii/api/Parser.java
+++ api/src/main/java/org/core_a/dev/wii/api/Parser.java
@@ -4,6 +4,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.LinkedList;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import static spark.Spark.*;
 import kr.ac.kaist.swrc.jhannanum.comm.Eojeol;
 import kr.ac.kaist.swrc.jhannanum.comm.Sentence;
@@ -29,6 +37,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Parser {
+    private final String USER_AGENT = "wii/1.0";
+
     public static void main(String[] args) {
         port(3002);
 
@@ -75,10 +85,10 @@ public class Parser {
     // to morpheme, parse context
     public static String parseRuliwebArticle(Document document) {
         Elements content = document.select("#gaiaViewCont");
-        Elements pTag = content.select("p");
+        Elements innerTag = content.select("table.read_cont_table");
 
         String contents = "";
-        for(Element e: pTag){
+        for(Element e: innerTag){
             contents += " " + e.text();
         }   
 
@@ -118,6 +128,9 @@ public class Parser {
             System.out.println("get, " + url);
             String article = parseRuliwebArticle(document);
             String title = parseRuliwebTitle(document);
+//            System.out.println("document, " + document);
+//            System.out.println("title, " + title);
+//            System.out.println("article, " + article);
 
             workflow.activateWorkflow(true);
 
@@ -175,5 +188,44 @@ public class Parser {
             //System.exit(0);
         }
         workflow.close();  	
+    }
+
+    private void sendPost(String param) throws Exception {
+
+        String url = "http://172.16.2.11:8080/article/import";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        //con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        //String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+        
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(param);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + param);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        
+        //print result
+        System.out.println(response.toString());
     }
 }
