984a2fbe27fb8c463c0bf986f6ef873c92118e2b
super-fish <super_fishz@core-a.org>
2015-10-25 06:43:39 +0900

    mysql report add

--- ajax/src/main/java/org/core_a/dev/wii/ajax/AnalyticsController.java
+++ ajax/src/main/java/org/core_a/dev/wii/ajax/AnalyticsController.java
@@ -32,14 +32,83 @@ import org.springframework.stereotype.Component;
 @Component
 @RestController
 public class AnalyticsController {
+    public class Report {
+        private int Amount;
+        private String StartAt;
+        private String EndAt;
+        private String Keyword;
+
+        public int getAmount() { return Amount; };
+        public void setAmount(int Amount) { this.Amount = Amount; };
+        public String getStartAt() { return StartAt; };
+        public void setStartAt(String StartAt) { this.StartAt = StartAt; };
+        public String getEndAt() { return EndAt; };
+        public void setEndAt(String EndAt) { this.EndAt = EndAt; };
+        public String getKeyword() { return Keyword; };
+        public void setKeyword(String Keyword) { this.Keyword = Keyword; };
+    }
+
     private static String driverNameHive = "org.apache.hive.jdbc.HiveDriver";
     private static String driverNameMysql = "com.mysql.jdbc.Driver";
 
     private final AtomicLong counter = new AtomicLong();
 
+    @RequestMapping("/report/{project}")
+    public String report(@PathVariable String project) {
+        Gson gson = new GsonBuilder().create();
+        List<Report> reports = getReport(project);
+
+        return gson.toJson(reports);
+    }
+
+    private List<Report> getReport(String project) {
+        //FIXME
+        String query = "SELECT amount, start_at, end_at, keyword FROM report WHERE project_seq = (select seq from project where name ='" + project + "')";
+        List<Report> reports = new ArrayList<Report>();
+
+        try {
+            try { // hive
+                Class.forName(driverNameMysql);
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                //System.exit(1);
+            }
+            try { // mysql
+                Class.forName(driverNameMysql);
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                //System.exit(1);
+            }
+
+            Connection conMysql = DriverManager.getConnection("jdbc:mysql://localhost:3306/wii", "root", "root");
+
+            Statement stmtMysql = conMysql.createStatement();
+
+            ResultSet res = null;
+            res = stmtMysql.executeQuery(query);
+
+            while (res.next()) {
+                Report report = new Report();
+
+                report.setAmount(res.getInt(1));
+                report.setStartAt(res.getString(2));
+                report.setEndAt(res.getString(3));
+                report.setKeyword(res.getString(4));
+                reports.add(report);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return reports;
+    }
+
     @Cacheable("articles")
     @RequestMapping("/article/{no}")
-    public String order(@PathVariable String no) {
+    public String article(@PathVariable String no) {
         //simulateSlowService();
         Gson gson = new GsonBuilder().create();
         Row row = getArticle(no);
@@ -96,5 +165,11 @@ public class AnalyticsController {
         }
 
         return row;
+    }
+
+    @Cacheable("users")
+    @RequestMapping("/user/{author}")
+    public String user(@PathVariable String author) {
+        return author;
     }
 }
